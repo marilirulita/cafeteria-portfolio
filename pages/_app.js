e81e46cd2984a455e556d4d66b3c9cb8d114916f
mar bautista <marilirulita@gmail.com>
2025-05-14 20:15:30 -0700

    add readme file and up scroll button

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import "@/styles/globals.css";
+import ScrollToTop from "@/components/ScrollToTop";
 
 import { Lora } from 'next/font/google'
 
 const lora = Lora({ subsets: ['latin'], weight: ['400', '700'] })
 
 export default function App({ Component, pageProps }) {
-  return <main className={lora.className}><Component {...pageProps} /></main>
+  return <main className={lora.className}><Component {...pageProps} /><ScrollToTop /></main>
 }
